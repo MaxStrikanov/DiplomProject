80af297af10c9eab908d82ad3b10603d0ba946de
Max Strikanov <maxvell.88@mail.ru>
2020-09-25 11:26:19 +0300

    Diplom

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 'use strict'
 
-import {popupCall, popupDiscount} from './modules/togglePopup';
+import {popupCall, popupDiscount, popupCheck, popupConsultation} from './modules/togglePopup';
 import accordionToggle from './modules/toggleAccordion';
 import showHiddenBlock from './modules/showHiddenBlock';
 import calc from './modules/calc';
+import sendForm from './modules/sendForm';
 
 popupCall();
 
 popupDiscount();
+
+popupCheck();
+
+popupConsultation()
 
 accordionToggle();
 
 showHiddenBlock();
 
 calc();
+
+sendForm();
--- src/modules/togglePopup.js
+++ src/modules/togglePopup.js
@@ -1,59 +1,80 @@
+'use strict'
+
+function popupClose (e)  {
+
+  let target = e.target;
+
+  if(target.classList.contains('popup-close')){
+    this.style.display = 'none';
+
+  } else {
+
+    target = target.closest('.popup-content')
+      if(!target){
+        this.style.display = 'none'
+      }
+  }
+}
+
 const popupCall = () => {
 
-  const popup = document.querySelector('.popup-call');
-  const callBtn = document.querySelectorAll('.call-btn');
-  
+    const popup = document.querySelector('.popup-call');
+    const callBtn = document.querySelectorAll('.call-btn');
+    
 
-  callBtn.forEach((elem) => {
-      elem.addEventListener('click', () => {
-      popup.style.display = 'block';  
+    callBtn.forEach((elem) => {
+        elem.addEventListener('click', () => {
+        popup.style.display = 'block';  
+      });
     });
-  });
 
-  popup.addEventListener('click', (e) => {
-
-    let target = e.target;
-
-    if(target.classList.contains('popup-close')){
-      popup.style.display = 'none';
-
-    } else {
-
-      target = target.closest('.popup-content')
-        if(!target){
-          popup.style.display = 'none'
-        }
-    }
-       
-  });
+    popup.addEventListener('click', popupClose);
 
 };
 
+
+
 const popupDiscount = () => {
-  const discountBtn = document.querySelectorAll('.discount-btn');
-  const popup = document.querySelector('.popup-discount');
-  
-  discountBtn.forEach( elem => elem.addEventListener('click', () => {
-      popup.style.display = 'block';
-  }));
+    const discountBtn = document.querySelectorAll('.discount-btn');
+    const popup = document.querySelector('.popup-discount');
+    
+    discountBtn.forEach( elem => elem.addEventListener( 'click', () => {
+        popup.style.display = 'block';
+    }));
 
-  popup.addEventListener('click', (e) => {
-
-      let target = e.target;
-
-      if(target.classList.contains('popup-close')){
-        popup.style.display = 'none';
-
-      } else {
-
-        target = target.closest('.popup-content')
-          if(!target){
-            popup.style.display = 'none'
-          }
-      }
-         
-    });
+    popup.addEventListener('click', popupClose);
 
 }
 
-export {popupCall, popupDiscount};
+const popupCheck = () => {
+    const checkBtn = document.querySelector('.check-btn');
+    const popup = document.querySelector('.popup-check');
+    
+    checkBtn.addEventListener('click', () => {
+        popup.style.display = 'block';
+    });
+
+    popup.addEventListener( 'click', popupClose);
+
+}
+
+const popupConsultation = () => {
+  const consultationBtn =document.querySelector('.consultation-btn');
+  const popup = document.querySelector('.popup-consultation');
+  const userQuest = document.querySelector('input[name="user_quest"]');
+  
+  consultationBtn.addEventListener('click', (e) => {
+    if (userQuest.value === ''){
+      userQuest.style.border = 'solid red';
+    } else {
+        popup.style.display = 'block';
+        userQuest.style.border = '';
+      }
+  }) 
+ 
+  popup.addEventListener('click', popupClose);
+
+}
+;
+
+export {popupCall, popupDiscount, popupCheck, popupConsultation};
